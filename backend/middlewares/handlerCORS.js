a6e75b9af731fc8371b8ee60d9ab94ed73bfade0
Dmitriy Minin <dbminin@yandex.ru>
2022-12-28 21:55:47 +0300

    fix: исправить CORS обработчик 4

--- backend/middlewares/handlerCORS.js
+++ backend/middlewares/handlerCORS.js
@@ -6,7 +6,7 @@ const allowedCors = [
 const handlerCORS = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', true);
   }
 
