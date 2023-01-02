2e60d3e52ae5d01b2186cc837db6303ae3487a77
Dmitriy Minin <dbminin@yandex.ru>
2023-01-02 18:50:52 +0300

    fix: добавить build в backend

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer root__footer">
-      <p lang="en" className="footer__text">&#169; 2022 Mesto Russia</p>
-    </footer>
-  )
-}
-
-export default Footer;
